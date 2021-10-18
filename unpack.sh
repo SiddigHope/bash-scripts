23355fdf9efc117f73c840996ed371ed545d3f41
Siddig A.Hamoda <seddig.hamouda@almarsa.co>
2021-10-18 11:18:26 +0200

    added the second task

--- unpack.sh
+++ unpack.sh
@@ -3,17 +3,85 @@
 # zipdir=/mnt/www/log/_tmp
 # cd $zipdir
 
-while getopts f:v:t: flag
+successful=0
+
+while getopts f:v:r: flag
 do
     case "${flag}" in
         f) files=${OPTARG};;
         v) show=${OPTARG};;
-        t) traverse=${OPTARG};;
+        r) traverse=${OPTARG};;
     esac
 done
 
+# for file in $traverse/*
+# do
+#     echo $file
+#     #whatever you need with "$file"
+# done
 
-successful=0
+# cd /traverse/
+
+ if [ -z "$traverse" ]; then
+        traverse=""
+else
+    # cd $traverse
+    for i in $traverse/*
+    do
+    extention=$(echo "$i" | sed 's/^.*\.//')
+    # echo $i
+    if [ "$extention" = "zip" ]; then 
+            decompress=$(eval "unzip -o $i -d $traverse")
+        if [ -z "$show" ]; then
+            if [ $? = 0 ]; then
+                successful=$((successful + 1));
+            fi
+        else
+            echo "Unpacking ${i}..."
+            successful=$((successful + 1));
+        fi
+    else
+    if [ "$extention" = "gz" ]; then
+            decompress=$(eval "gunzip --f -d -q -t -c $i > $i")
+        if [ -z "$show" ]; then
+            if [ $? = 0 ]; then
+                successful=$((successful + 1));
+            fi
+        else
+            echo "Unpacking ${i}..."
+            successful=$((successful + 1));
+        fi
+    else
+    if [ "$extention" = "bz2" ]; then
+            decompress=$(eval "bunzip2 -d -q $i -c $traverse")
+        if [ -z "$show" ]; then
+            if [ $? = 0 ]; then
+                successful=$((successful + 1))
+            fi
+        else
+            echo "Unpacking ${i}..."
+            successful=$((successful + 1));
+        fi
+    else
+    if [ "$extention" = "cmpr" ]; then
+            decompress=$(eval "uncompress -f -d -q -t -c $i > $i")
+        if [ -z "$show" ]; then
+            if [ $? = 0 ]; then
+                successful=$((successful + 1))
+            fi
+        else
+            echo "Unpacking ${i}..."
+            successful=$((successful + 1));
+        fi
+    fi
+    fi
+    fi
+    fi
+    done
+fi
+
+cd ..
+
 for i in "$@"
 do
 extention=$(echo "$i" | sed 's/^.*\.//')
